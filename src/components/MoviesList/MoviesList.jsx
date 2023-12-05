ac63b6e8895ce23e1b289172fbf7c07ee04328c9
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2023-12-05 21:59:42 +0200

    comit7

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,25 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Item, List } from './MoviesList.styled';
+import { ImgContainer, Item, List, OriginTitle } from './MoviesList.styled';
 
 export const MoviesList = ({ movies }) => {
   const location = useLocation();
   return (
     <div>
       <List>
-        {movies.map(({ id, original_title }) => (
+        {movies.map(({ id, original_title, poster_path }) => (
           <Item key={id}>
             <Link state={{ from: location }} to={`/movies/${id}`}>
-              {original_title}
+              {poster_path && (
+                <ImgContainer>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${poster_path}`}
+                    alt="poster"
+                    width={200}
+                    height={290}
+                  />
+                </ImgContainer>
+              )}
+              <OriginTitle>{original_title}</OriginTitle>
             </Link>
           </Item>
         ))}
--- src/components/MoviesList/MoviesList.styled.js
+++ src/components/MoviesList/MoviesList.styled.js
@@ -3,8 +3,27 @@ import { styled } from 'styled-components';
 export const List = styled.ul`
   margin: auto;
   margin-left: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 15px;
 `;
 
 export const Item = styled.li`
-  margin-bottom: 15px;
+  max-width: 200px;
+  align-items: center;
+  border: 1px solid #000;
+`;
+
+export const OriginTitle = styled.p`
+  margin: auto;
+  height: 65px;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const ImgContainer = styled.div`
+  border-bottom: 1px solid #3333;
 `;
