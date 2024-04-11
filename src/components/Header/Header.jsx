1e1f21e73b9e7ccd32a66d5552e9d51f5d9651c1
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-04-12 00:10:13 +0300

    styled navigate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { HeaderList, NavHeader } from './Header.styled';
+import { styled } from 'styled-components';
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: #333;
+  padding: 4px 16px;
+  border-radius: 10px;
+  background-color: #899599;
+  font-weight: bold;
+  transition: color 500ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    background-color: #49c2ee;
+    color: white;
+  }
+
+  &.active {
+    background-color: #98d3ec;
+    color: white;
+  }
+`;
 
 export const Header = () => {
   return (
     <NavHeader>
       <HeaderList>
         <li>
-          <NavLink className="nav-link " to="/">
-            Home
-          </NavLink>
+          <StyledNavLink to="/">Home</StyledNavLink>
         </li>
         <li>
-          <NavLink className="nav-link " to="/movies">
-            Movies
-          </NavLink>
+          <StyledNavLink to="/movies">Movies</StyledNavLink>
         </li>
       </HeaderList>
     </NavHeader>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,16 +3,25 @@ import { styled } from 'styled-components';
 export const NavHeader = styled.nav`
   display: flex;
   text-align: center;
+  align-items: center;
   height: 40px;
   margin: 0 auto;
+  background-color: black;
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
 `;
 
 export const HeaderList = styled.ul`
-  margin: auto;
-  margin-left: 20px;
+  padding-left: 100px;
+
   display: flex;
   justify-content: center;
   gap: 20px;
   text-align: center;
 `;
+
+// export const HeaderItem = styled.li`
+//   width: 60px;
+//   height: 25px;
+//   background-color: #a41414;
+//   border-radius: 10px;
+// `;
